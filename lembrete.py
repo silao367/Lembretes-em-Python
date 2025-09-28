d5a8d9526c698da2a777be7b49bc2c94be0a98b4
silao367 <silasgah37@gmail.com>
2025-09-27 23:33:52 -0300

    Pirmeiro de muitos

---
+++ lembrete.py
@@ -0,0 +1,14 @@
+import time 
+
+from plyer import notification
+
+def lembrete():
+    notification.notify(
+        title="Ja se Hidratou Hoje Estrupicio?",
+        message="Hora de beber agua 💧",
+        timeout=10
+    )
+
+while True:
+    lembrete()
+    time.sleep(3600)    
